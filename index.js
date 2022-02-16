2fdcd55161dec3872c72994865a4cde75e99d3b9
tylergubser <tylergubser2200@gmail.com>
2022-02-16 12:02:22 -0600

    commit tree ef8da9e58b61f1efb7bda59b388de856e52f7f38

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching(array, string) {
+    return array.filter(element => element.toLowerCase() || element == string)
+    
+}
+
+function fuzzyMatch(array, stringMatch) {
+    return array.filter(element => element.startsWith(stringMatch))
+}
+
+function matchName(array, string) {
+    return array.filter(el => el.name === string)
+
+}
+
+    
+    
